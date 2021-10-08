6b1c884460102845b1d483735e3b466af88d5f43
krishna <patnaik.krishnaprasad9@gmail.com>
2021-10-08 10:47:15 +0530

    init

---
+++ src/main/java/com/example/easyrentalbackend/EasyrentalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.easyrentalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyrentalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyrentalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easyrentalbackend/EasyrentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easyrentalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyrentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
